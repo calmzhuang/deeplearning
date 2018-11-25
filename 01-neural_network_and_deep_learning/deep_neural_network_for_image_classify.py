0405ebb771742dccba5faada2aad5fbd06337fe2
calmzhuang <calmzhuang@163.com>
2018-11-25 21:26:36 +0800

    Modify format of deep_neural_network_for_image_classify.py

--- 01-neural_network_and_deep_learning/deep_neural_network_for_image_classify.py
+++ 01-neural_network_and_deep_learning/deep_neural_network_for_image_classify.py
@@ -34,12 +34,10 @@ def get_data():
     # Load the data.
     train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
-
     # Reshape the training and test examples
     train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
     # The "-1" makes reshape flatten the remaining dimensions
     test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
-
 
     # Standardize data to have feature values between 0 and 1.
     train_x = train_x_flatten/255.
