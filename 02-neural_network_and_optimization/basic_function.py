10d0e93825d0c0d818bd9bc4692d06faa258ed41
calmzhuang <calmzhuang@163.com>
2018-12-16 23:25:08 +0800

    add some optimizer methods and prepare for softmax

--- 02-neural_network_and_optimization/basic_function.py
+++ 02-neural_network_and_optimization/basic_function.py
@@ -27,6 +27,42 @@ def initialize_parameters_deep(layer_dims):
     return parameters
 
 
+def initialize_adam(parameters):
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL"
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+
+    Returns:
+    v -- python dictionary that will contain the exponentially weighted average of the gradient.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v = {}
+    s = {}
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(L):
+        ### START CODE HERE ### (approx. 4 lines)
+        v["dW" + str(l + 1)] = np.zeros((parameters["W" + str(l + 1)].shape[0], parameters["W" + str(l + 1)].shape[1]))
+        v["db" + str(l + 1)] = np.zeros((parameters["b" + str(l + 1)].shape[0], 1))
+        s["dW" + str(l + 1)] = np.zeros((parameters["W" + str(l + 1)].shape[0], parameters["W" + str(l + 1)].shape[1]))
+        s["db" + str(l + 1)] = np.zeros((parameters["b" + str(l + 1)].shape[0], 1))
+    ### END CODE HERE ###
+
+    return v, s
+
+
 def sigmoid(Z):
     """
     Implements the sigmoid activation in numpy
@@ -39,13 +75,13 @@ def sigmoid(Z):
     cache -- returns Z as well, useful during backpropagation
     """
 
-    A = 1/(1 + np.exp(-Z))
+    a_cap = 1/(1 + np.exp(-Z))
     cache = Z
 
-    return A, cache
+    return a_cap, cache
 
 
-def relu(Z):
+def relu(z_cap):
     """
     Implement the RELU function.
 
@@ -57,15 +93,15 @@ def relu(Z):
     cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
     """
 
-    A = np.maximum(0, Z)
+    a_cap = np.maximum(0, z_cap)
 
-    assert (A.shape == Z.shape)
+    assert (a_cap.shape == z_cap.shape)
 
-    cache = Z
-    return A, cache
+    cache = z_cap
+    return a_cap, cache
 
 
-def linear_forward(A, W, b):
+def linear_forward(a_cap, w_cap, b):
     """
     Implement the linear part of a layer's forward propagation.
 
@@ -79,15 +115,15 @@ def linear_forward(A, W, b):
     cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
     """
 
-    Z = W.dot(A) + b
+    z_cap = w_cap.dot(a_cap) + b
 
-    assert (Z.shape == (W.shape[0], A.shape[1]))
-    cache = (A, W, b)
+    assert (z_cap.shape == (w_cap.shape[0], a_cap.shape[1]))
+    cache = (a_cap, w_cap, b)
 
-    return Z, cache
+    return z_cap, cache
 
 
-def linear_activation_forward(A_prev, W, b, activation):
+def linear_activation_forward(A_prev, W, b, activation, keep_prob):
     """
     Implement the forward propagation for the LINEAR->ACTIVATION layer
 
@@ -109,14 +145,17 @@ def linear_activation_forward(A_prev, W, b, activation):
     elif activation == "relu":
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
         A, activation_cache = relu(z)
-
+    d_cap = np.random.rand(A.shape[0], A.shape[1])
+    d_cap = d_cap < keep_prob
+    A = np.multiply(A, d_cap)
+    A = A/keep_prob
     assert (A.shape == (W.shape[0], A_prev.shape[1]))
     cache = (linear_cache, activation_cache)
 
     return A, cache
 
 
-def L_model_forward(X, parameters):
+def L_model_forward(X, parameters, keep_prob):
     """
     Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
 
@@ -139,11 +178,12 @@ def L_model_forward(X, parameters):
     for l in range(1, L):
         A_prev = A
         A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)],
-                                             activation="relu")
+                                             activation="relu", keep_prob=keep_prob[0])
         caches.append(cache)
 
     # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
-    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation="sigmoid")
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation="sigmoid",
+                                          keep_prob=keep_prob[1])
     caches.append(cache)
 
     assert (AL.shape == (1, X.shape[1]))
@@ -166,7 +206,7 @@ def compute_cost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T))
+    cost = (1. / m) * np.sum(np.sum(-np.multiply(Y, np.log(AL))))
 
     cost = np.squeeze(cost)  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
     assert (cost.shape == ())
@@ -267,7 +307,7 @@ def linear_activation_backward(dA, cache, activation):
         dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
     elif activation == "sigmoid":
-        dZ = sigmoid_backward(dA, activation_cache)
+        dZ = dA
         dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
     return dA_prev, dW, db
@@ -296,11 +336,11 @@ def L_model_backward(AL, Y, caches):
     Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
 
     # Initializing the backpropagation
-    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    dZL = AL - Y
 
     # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
     current_cache = caches[L - 1]
-    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache,
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dZL, current_cache,
                                                                                                   activation="sigmoid")
 
     for l in reversed(range(L - 1)):
@@ -315,28 +355,70 @@ def L_model_backward(AL, Y, caches):
     return grads
 
 
-def update_parameters(parameters, grads, learning_rate):
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
+                                beta1=0.9, beta2=0.999, epsilon=1e-8):
     """
-    Update parameters using gradient descent
+    Update parameters using Adam
 
-    :param AL, Y, caches:
-    parameters -- python dictionary containing your parameters
-    grads -- python dictionary containing your gradients, output of L_model_backward
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    learning_rate -- the learning rate, scalar.
+    beta1 -- Exponential decay hyperparameter for the first moment estimates
+    beta2 -- Exponential decay hyperparameter for the second moment estimates
+    epsilon -- hyperparameter preventing division by zero in Adam updates
 
-    :return:
+    Returns:
     parameters -- python dictionary containing your updated parameters
-                  parameters["W" + str(l)] = ...
-                  parameters["b" + str(l)] = ...
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
     """
 
-    L = len(parameters) // 2  # number of layers in the neural network
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v_corrected = {}  # Initializing first moment estimate, python dictionary
+    s_corrected = {}  # Initializing second moment estimate, python dictionary
 
-    # Update rule for each parameter. Use a for loop.
+    # Perform Adam update on all parameters
     for l in range(L):
-        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
-        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+        # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
+        ### START CODE HERE ### (approx. 2 lines)
+        v["dW" + str(l + 1)] = beta1 * v["dW" + str(l + 1)] + (1 - beta1) * grads["dW" + str(l + 1)]
+        v["db" + str(l + 1)] = beta1 * v["db" + str(l + 1)] + (1 - beta1) * grads["db" + str(l + 1)]
+        ### END CODE HERE ###
 
-    return parameters
+        # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        v_corrected["dW" + str(l + 1)] = v["dW" + str(l + 1)] / (1 - (beta1) ** t)
+        v_corrected["db" + str(l + 1)] = v["db" + str(l + 1)] / (1 - (beta1) ** t)
+        ### END CODE HERE ###
+
+        # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
+        ### START CODE HERE ### (approx. 2 lines)
+        s["dW" + str(l + 1)] = beta2 * s["dW" + str(l + 1)] + (1 - beta2) * grads["dW" + str(l + 1)] ** 2
+        s["db" + str(l + 1)] = beta2 * s["db" + str(l + 1)] + (1 - beta2) * grads["db" + str(l + 1)] ** 2
+        ### END CODE HERE ###
+
+        # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        s_corrected["dW" + str(l + 1)] = s["dW" + str(l + 1)] / (1 - (beta2) ** t)
+        s_corrected["db" + str(l + 1)] = s["db" + str(l + 1)] / (1 - (beta2) ** t)
+        ### END CODE HERE ###
+
+        # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
+        ### START CODE HERE ### (approx. 2 lines)
+        parameters["W" + str(l + 1)] -= learning_rate * v_corrected["dW" + str(l + 1)] / (
+                    np.sqrt(s_corrected["dW" + str(l + 1)]) + epsilon)
+        parameters["b" + str(l + 1)] -= learning_rate * v_corrected["db" + str(l + 1)] / (
+                    np.sqrt(s_corrected["db" + str(l + 1)]) + epsilon)
+        ### END CODE HERE ###
+
+    return parameters, v, s
 
 
 def predict(X, y, parameters):
@@ -356,7 +438,7 @@ def predict(X, y, parameters):
     p = np.zeros((1, m))
 
     # Forward propagation
-    probas, caches = L_model_forward(X, parameters)
+    probas, caches = L_model_forward(X, parameters, keep_prob=[1, 1])
 
     # convert probas to 0/1 predictions
     for i in range(0, probas.shape[1]):
@@ -371,6 +453,137 @@ def predict(X, y, parameters):
     print("Accuracy: " + str(np.sum((p == y) / m)))
 
     return p
+
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7):
+    """
+    Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
+
+    Arguments:
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+    grad -- output of backward_propagation_n, contains gradients of the cost with respect to the parameters.
+    x -- input datapoint, of shape (input size, 1)
+    y -- true "label"
+    epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+
+    Returns:
+    difference -- difference (2) between the approximated gradient and the backward propagation gradient
+    """
+
+    # Set-up variables
+    parameters_values, _ = dictionary_to_vector(parameters)
+    grad = gradients_to_vector(gradients)
+    num_parameters = parameters_values.shape[0]
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+
+    # Compute gradapprox
+    for i in range(num_parameters):
+        # Compute J_plus[i]. Inputs: "parameters_values, epsilon". Output = "J_plus[i]".
+        # "_" is used because the function you have to outputs two parameters but we only care about the first one
+        ### START CODE HERE ### (approx. 3 lines)
+        thetaplus = np.copy(parameters_values)  # Step 1
+        thetaplus[i][0] += epsilon  # Step 2
+        AL, _ = L_model_forward(X, vector_to_dictionary(thetaplus), keep_prob=[1, 1])  # Step 3
+        J_plus[i], _ = compute_cost(AL, Y)  # Step 4
+        ### END CODE HERE ###
+
+        # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
+        ### START CODE HERE ### (approx. 3 lines)
+        thetaminus = np.copy(parameters_values)  # Step 1
+        thetaminus[i][0] -= epsilon  # Step 2
+        AL, _ = L_model_forward(X, vector_to_dictionary(thetaminus), keep_prob=[1, 1])  # Step 3
+        J_plus[i], _ = compute_cost(AL, Y)  # Step 4
+        ### END CODE HERE ###
+
+        # Compute gradapprox[i]
+        ### START CODE HERE ### (approx. 1 line)
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+        ### END CODE HERE ###
+
+    # Compare gradapprox to backward propagation gradients by computing difference.
+    ### START CODE HERE ### (approx. 1 line)
+    numerator = np.linalg.norm(grad - gradapprox)  # Step 1'
+    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)  # Step 2'
+    difference = numerator / denominator  # Step 3'
+    ### END CODE HERE ###
+
+    if difference > 2e-7:
+        print(
+            "\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+    else:
+        print(
+            "\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+
+    return difference
+
+
+def dictionary_to_vector(parameters):
+    """
+    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    """
+    keys = []
+    count = 0
+    num_parameter = len(parameters)/3
+    key_val = []
+    for l in range(int(num_parameter)):
+        key_val.append("W" + str(l + 1))
+        key_val.append("b" + str(l + 1))
+    for key in key_val:
+
+        # flatten parameter
+        new_vector = np.reshape(parameters[key], (-1, 1))
+        keys = keys + [key] * new_vector.shape[0]
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta, keys
+
+
+def vector_to_dictionary(theta, parameters):
+    """
+    Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+    """
+    num_parameters = len(parameters)
+    start_index = 0
+    for i in range(num_parameters):
+        W_one = parameters["W" + str(i + 1)].shape[0]
+        W_two = parameters["W" + str(i + 1)].shape[1]
+        one_index = W_one*W_two + start_index
+        two_index = one_index + W_one
+        parameters["W" + str(i + 1)] = theta[start_index:one_index].reshape((W_one, W_two))
+        parameters["W" + str(i + 1)] = theta[one_index:two_index].reshape((W_one, 1))
+        start_index = two_index
+    return parameters
+
+
+def gradients_to_vector(gradients):
+    """
+    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
+    """
+
+    count = 0
+    num_parameter = len(gradients) / 3
+    key_val = []
+    for l in range(int(num_parameter)):
+        key_val.append("W" + str(l + 1))
+        key_val.append("b" + str(l + 1))
+    for key in key_val:
+        # flatten parameter
+        new_vector = np.reshape(gradients[key], (-1, 1))
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta
 
 
 def print_mislabeled_images(classes, X, y, p):
@@ -395,6 +608,7 @@ def print_mislabeled_images(classes, X, y, p):
         plt.title(
             "Prediction: " + classes[int(p[0, index])].decode("utf-8") + " \n Class: " + classes[y[0, index]].decode(
                 "utf-8"))
+
 
 if __name__ == '__main__':
     pass
